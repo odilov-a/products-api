82bbdb59d722bc076cb64091b6309b4ae9d86b3d
odilov-a <akbarjonodilov01@gmail.com>
2024-05-14 10:58:42 +0500

    chore: Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,8 @@ const authMiddleware = require("../middlewares/auth.middleware.js");
 const userController = require("../controller/user.controller.js");
 const userRoutes = Router();
 
-userRoutes.get("/get-me", authMiddleware, userController.getMe);
 userRoutes.post("/login", userController.login);
+userRoutes.get("/get-me", authMiddleware, userController.getMe);
 userRoutes.put("/update-user", authMiddleware, userController.updateUser);
 
 module.exports = userRoutes;
